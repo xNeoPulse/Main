bbafe5ad468f5fc1e341b3b9d6e394a718b2d919
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-07 16:01:06 +0800

    Create Changelogs.lua

---
+++ Game Data/Blair/Changelogs.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        ["Version"] = "1.0.0";
+        ["Status"] = "Beta";
+        ["Logs"] = {
+            "Added Custom Speed";
+            "Added Status/Info Box";
+            "Added ESP";
+        };
+    };
+}
