266789557efab05b42908190c3e68d987790f84f
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-07 17:27:24 +0800

    Update Changelogs.lua

--- Game Data/Blair/Changelogs.lua
+++ Game Data/Blair/Changelogs.lua
@@ -3,9 +3,13 @@ return {
         ["Version"] = "1.0.0";
         ["Status"] = "Beta";
         ["Logs"] = {
+            "Added Custom Light";
             "Added Custom Speed";
+            "Added Fullbright";
+            "Added Door No Clip";
             "Added Status/Info Box";
             "Added ESP";
+            "Added Freecam";
         };
     };
 }
