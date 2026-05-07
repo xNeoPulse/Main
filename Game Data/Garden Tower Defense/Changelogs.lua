03bd5da62057c6f45a50d17709965cac804c4688
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-07 14:49:57 +0800

    Update Changelogs.lua

--- Game Data/Garden Tower Defense/Changelogs.lua
+++ Game Data/Garden Tower Defense/Changelogs.lua
@@ -1,6 +1,17 @@
 return {
-    ["1.0.0"] = {
-        "Added Auto Lobby, Auto Difficulty, Auto Next";
-        "Added Macro, Macro Maps";
+    {
+        ["Version"] = "1.0.0";
+        ["Status"] = "Beta";
+        ["Logs"] = {
+            "Added Auto Lobby";
+            "Added Auto Difficulty";
+            "Added Auto Game Speed";
+            "Added Auto Next";
+            "Added Macro";
+            "Added Macro Maps";
+            "Added Unit/Crate Box Shop";
+            "Added Buy Limited Stock";
+            "Added Traders Offers";
+        };
     };
 }
