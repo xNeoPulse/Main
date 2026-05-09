50c8a44978302254901a01861306dbaac92d74c8
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-09 14:30:42 +0800

    Create Changelogs.lua

---
+++ Game Data/Dive Down/Changelogs.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        ["Version"] = "1.0.0";
+        ["Date"] = "2026-05-09";
+        ["Status"] = "Beta";
+        ["Logs"] = {
+            "Added Auto Catch";
+            "Added Auto Sell";
+            "Added Auto Buy";
+            "Added ESP";
+            "Added Teleporters";
+            "Added Hide Fish";
+        };
+    };
+}
