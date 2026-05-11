ef8a4c7c9e178bd17e793c9babeb15368b5fc967
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-12 04:39:13 +0800

    Create Data.lua

---
+++ Game Data/Blair/Data.lua
@@ -0,0 +1,23 @@
+local data = {};
+
+data["TOOLS"] = {
+    "Crucifix",
+    "EMF Reader",
+    "Ghost Writing Book",
+    "GlowStick",
+    "Incense Burner",
+    "Lighter",
+    "Parabolic Microphone",
+    "Photo Camera",
+    "SLS Camera",
+    "Salt",
+    "Sanity Soda",
+    "Spirit Box",
+    "Strong Flashlight",
+    "Thermometer",
+    "Trail Camera",
+    "UV Light",
+    "Video Camera"
+};
+
+return data;
