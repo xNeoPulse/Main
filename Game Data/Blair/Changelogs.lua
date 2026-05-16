ae92812648220e59b945464308e23975e50a6918
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-16 23:43:06 +0800

    Update Changelogs.lua

--- Game Data/Blair/Changelogs.lua
+++ Game Data/Blair/Changelogs.lua
@@ -1,5 +1,15 @@
 return {
     {
+        ["Version"] = "1.0.1";
+        ["Date"] = "2026-05-16";
+        ["Status"] = "Beta";
+        ["Logs"] = {
+            "Added Cursed Object Info";
+            "Added Cursed Object ESP";
+            "Added Generators to ESP";
+            "Added BooBoo Doll to ESP";
+        };
+    },{
         ["Version"] = "1.0.0";
         ["Date"] = "2026-05-08";
         ["Status"] = "Beta";
