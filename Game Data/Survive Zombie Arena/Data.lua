f1f57eefbfd8eb4e0316dc5c4fc2e14b2c5429b9
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-22 20:08:00 +0800

    Create Data.lua

---
+++ Game Data/Survive Zombie Arena/Data.lua
@@ -0,0 +1,61 @@
+local data = {};
+
+data["WEAPONS"] = {
+    ["Pistol"] = { Tier = 1; Damage = 17 };
+    ["ShotGun"] = { Tier = 2; Damage = 40 };
+    ["Rifle"] = { Tier = 3; Damage = 60 };
+    ["Minigun"] = { Tier = 4; Damage = 13 };
+    ["Revolver"] = { Tier = 5; Damage = 65 };
+    ["DualPistols"] = { Tier = 6; Damage = 35 };
+    ["SMG"] = { Tier = 7; Damage = 12 };
+    ["CombatShotgun"] = { Tier = 8; Damage = 55 };
+    ["BurstRifle"] = { Tier = 9; Damage = 160 };
+    ["AK47"] = { Tier = 10; Damage = 90 };
+    ["Sniper"] = { Tier = 11; Damage = 500 };
+    ["HeavyRifle"] = { Tier = 12; Damage = 155 };
+    ["Flamethrower"] = { Tier = 13; Damage = 104 };
+    ["MP5"] = { Tier = 14; Damage = 20 };
+    ["USPS"] = { Tier = 15; Damage = 50 };
+    ["GoldenAK47"] = { Tier = 16; Damage = 450 };
+    ["EmberSMG"] = { Tier = 17; Damage = 275 };
+    ["LavaRifle"] = { Tier = 18; Damage = 523 };
+    ["CoreBreaker"] = { Tier = 19; Damage = 629 };
+    ["LavaBow"] = { Tier = 20; Damage = 2160 };
+    ["InfernoMinigun"] = { Tier = 21; Damage = 750 };
+    ["LavaGatling"] = { Tier = 22; Damage = 880 };
+    ["GumdropBlaster"] = { Tier = 23; Damage = 750 };
+    ["ArticStriker"] = { Tier = 24; Damage = 750 };
+    ["GalacticWeaver"] = { Tier = 25; Damage = 17 };
+    ["WorldEnder"] = { Tier = 26; Damage = 17 };
+    ["TommyGun"] = { Tier = 27; Damage = 70 };
+    ["GrenadeLauncher"] = { Damage = 600; };
+}
+
+data["GEARS"] = {
+    "AutoTurret";
+    "Barricade";
+    "BladeFury";
+    "Bunker";
+    "Cloak";
+    "Deadeye";
+    "DeathNova";
+    "Drone";
+    "FlamethrowerTurret";
+    "FragGrenade";
+    "HealingStation";
+    "Landmine";
+    "LaserTurret";
+    "MendingTower";
+    "Molotov";
+    "RaiseUndead";
+    "ShockwaveMine";
+    "Shuriken";
+    "SoulHarvester";
+    "Spikes";
+    "SteelBarricade";
+    "StimShot";
+    "TargetMark";
+    "VanguardTurret";
+}
+
+return data;
