892bf884078e9946098917ce4f93e3ae47838c15
xNeoPulse <charlesgorre2001@gmail.com>
2026-05-26 07:21:12 +0800

    Create Changelogs.lua

---
+++ Game Data/Demonology/Changelogs.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        ["Version"] = "1.0.0";
+        ["Date"] = 1778212800;
+        ["Status"] = "Beta";
+        ["Logs"] = {
+            "Initial Release";
+        };
+    };
+}
