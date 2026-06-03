79c4d3ee3305d56b36112784e0f769d723763222
xNeoPulse <charlesgorre2001@gmail.com>
2026-06-03 22:48:01 +0800

    Update Obsidian.lua

--- Obsidian.lua
+++ Obsidian.lua
@@ -1788,6 +1788,7 @@ function Library:AddDraggableMenu(Name: string)
 	})
 
 	local Container = New("Frame", {
+		AutomaticSize = Enum.AutomaticSize.XY,
 		BackgroundTransparency = 1,
 		Position = UDim2.fromOffset(0, 35),
 		Size = UDim2.new(1, 0, 1, -35),
