6b8945b24ae4771dd1fb1faacde6d8e452e4141d
xNeoPulse <charlesgorre2001@gmail.com>
2026-07-04 00:03:44 +0800

    Update ObsidianNew.lua

--- ObsidianNew.lua
+++ ObsidianNew.lua
@@ -222,20 +222,20 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-        BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
-        Font = Font.fromEnum(Enum.Font.Code),
+		BackgroundColor = Color3.fromRGB(10, 10, 10),
+		MainColor = Color3.fromRGB(20, 20, 20),
+		AccentColor = Color3.fromRGB(0, 255, 209),
+		OutlineColor = Color3.fromRGB(40, 40, 40),
+		FontColor = Color3.new(1, 1, 1),
+		Font = Font.fromEnum(Enum.Font.Code),
 
-        RedColor = Color3.fromRGB(255, 50, 50),
-        DestructiveColor = Color3.fromRGB(220, 38, 38),
-        DarkColor = Color3.new(0, 0, 0),
-        WhiteColor = Color3.new(1, 1, 1),
+		RedColor = Color3.fromRGB(255, 50, 50),
+		DestructiveColor = Color3.fromRGB(220, 38, 38),
+		DarkColor = Color3.new(0, 0, 0),
+		WhiteColor = Color3.new(1, 1, 1),
 
-        BackgroundImage = ""
-    },
+		BackgroundImage = "",
+	},
 
     Registry = {},
 	Scales = {},
