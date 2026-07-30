94908e20530a42b85a242a7dd80aca8e0cf13026
xNeoPulse <charlesgorre2001@gmail.com>
2026-07-30 19:26:59 +0800

    Update Obsidian.lua

--- Obsidian.lua
+++ Obsidian.lua
@@ -1152,7 +1152,7 @@ type IconModule = {
 
 local FetchIcons, Icons = pcall(function()
     return (loadstring(
-        game:HttpGet("https://raw.githubusercontent.com/deividcomsono/lucide-roblox-direct/refs/heads/main/source.lua")
+        game:HttpGet("https://gitlab.com/upio/lucide-roblox-direct/-/raw/main/source.lua")
     ) :: () -> IconModule)()
 end)
 
